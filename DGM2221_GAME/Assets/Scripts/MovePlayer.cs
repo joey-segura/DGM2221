dc7767aa2818d433424fedd94aa0fb7b5c8413ea
Joey Segura <10735119@my.uvu.edu>
2020-09-30 21:04:49 -0600

    WalkRun

---
+++ DGM2221_GAME/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float moveSpeed;
+    private float defaultSpeed = 5f, sprintSpeed = 10f;
+
+    private Rigidbody rbody;
+   
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    
+   
+    void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * moveSpeed * Time.fixedDeltaTime;
+        
+        rbody.MovePosition(transform.position + movement);
+
+        if (Input.anyKey)
+        {
+            transform.LookAt(transform.position + new Vector3(movement.x, 0, movement.z));
+        }
+        
+       
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = defaultSpeed;
+        }
+    }
+}
