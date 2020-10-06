8693e03a44d0da9cc91a365bc98f85a0338f4a2b
Joey Segura <10735119@my.uvu.edu>
2020-10-05 18:50:47 -0600

    Jump_DoubleJump

---
+++ DGM2221_GAME/Assets/Scripts/Jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public GameObject groundCheck;
+
+    public float jumpStrength = 5f;
+
+    public int jumpCount = 0, jumpMax = 2;
+    
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        groundCheck = GameObject.Find("/Player/GroundCheck");
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpMax)
+        {
+            rb.velocity = (Vector3.up * jumpStrength);
+            jumpCount++;
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        jumpCount = 0;
+    }
+}
