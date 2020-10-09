0f0847e41854a21fc355fd5adddc28eb80ae576a
Joey Segura <10735119@my.uvu.edu>
2020-10-08 20:30:50 -0600

    Merge

--- DGM2221_GAME/Assets/Scripts/Jump.cs
+++ DGM2221_GAME/Assets/Scripts/Jump.cs
@@ -8,7 +8,11 @@ public class Jump : MonoBehaviour
     public Rigidbody rb;
 
     public GameObject groundCheck;
-
+    
+    private float moveSpeed;
+    public float defaultSpeed = 3f, sprintSpeed = 6f;
+    
+    public float gravity;
     public float jumpStrength = 5f;
 
     public int jumpCount = 0, jumpMax = 2;
@@ -23,6 +27,37 @@ public class Jump : MonoBehaviour
 
     void Update()
     {
+        //WALKING
+
+        rb.AddForce(Vector3.down * gravity);
+        
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * moveSpeed * Time.deltaTime;
+        
+        rb.MovePosition(transform.position + movement);
+
+        // IF PLAYER IS NOT GROUNDED DO NOT TAKE ANY INPUT, THIS WILL CONTINUE TO LET THE PLAYER TUMBLE IN THE AIR IGNORING THIS LOOKAT FUNCTION.
+        if (Input.anyKey)
+        {
+            transform.LookAt(transform.position + new Vector3(hAxis, 0, vAxis));
+        }
+        
+        //SPRINTING
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = defaultSpeed;
+        }
+        
+        
+        //JUMPING
+        
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpMax)
         {
             rb.velocity = (Vector3.up * jumpStrength);
